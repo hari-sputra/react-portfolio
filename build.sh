2f95d5ca87752df04048e04cdf14e3f6707526a7
hari saputra <hari.saputra.dev@gmail.com>
2025-02-12 15:59:04 +0700

    [update] build sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -f Dockerfile -t portfolio-frontend/web:1.0.0-dev  . && docker compose up -d --force-recreate
+docker build -f Dockerfile -t portfolio-frontend/web:1.0.0-dev  . && docker-compose up -d --force-recreate
